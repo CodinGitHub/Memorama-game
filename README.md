04e548f11c0eea64cdc2f04740f452ec86e161a5
CodinGitHub <gms2programador@gmail.com>
2022-04-25 09:15:03 -0500

    grid card generated

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Memorama-game
+
+1. Seleccionar el elemento grid
+2. Determinar que contenedor de tarjeta hizo el clic
+3. Poner estilos a las tarjetas dentro de ese div
--- main.js
+++ main.js
@@ -7,43 +7,72 @@ let classList = [
   'nes-squirtle',
 ]
 
+card = document.getElementById('card')
+front = document.getElementsByClassName('front');
+back = document.getElementsByClassName('back');
+
 // Card Generator
-/*
+
 for(i=0; i<classList.length; i++){
     cardGrid.innerHTML += `
-    <div class="card-container" id="${i}">
-        <div id="front" class="card front nes-container is-rounded">
-            <i class="${classList[i]}"></i>
+    <div class="card-container" id="card${i}">
+        <div class="card back nes-container is-rounded">
+            <li class="nes-pokeball"></li>
         </div>
-        <div id="back" class="card back nes-container is-rounded">
-            <i class="nes-pokeball"></i>
-        </div>
+        <div class="card front nes-container is-rounded"></div>
     </div>`
-    
+    front[i].style.transform = 'rotateY(180deg)';
+    back[i].style.transform = 'rotateY(0deg)';   
 }
+
+/*
+cardGrid.innerHTML = `
+    <div class="card-container" id="card0">
+        <div class="card back nes-container is-rounded">
+            <li class="nes-pokeball"></li>
+        </div>
+        <div class="card front nes-container is-rounded"></div>
+    </div>
+`
 */
 
-cardGrid.innerHTML = `
-    <div class="card-container" id="1">
-        <div id="front" class="card front nes-container is-rounded">
-            <i class="${classList[1]}"></i>
-        </div>
-        <div id="back" class="card back nes-container is-rounded">
-            <i class="nes-pokeball"></i>
-        </div>
-    </div>`
+cardGrid.addEventListener('click', (event)=>{
+    console.log(event)
+    if(event.srcElement.offsetParent.id == ''){
+        console.log ('hiciste clic en la imagen')
+    }else{
+        console.log(event.srcElement.offsetParent.id)
+    }
+});
 
-card = document.getElementsByClassName('card-container')
+
+function flipCard (id){
+    currentCard = document.getElementById(`card${id}`)
+    currentCard.addEventListener('click', (event)=>{
+        // console.log(event)
+        // console.log(event.srcElement.offsetParent.id)
+        front[id].style.transform = 'rotateY(0deg)';
+        back[id].style.transform = 'rotateY(180deg)';
+        setTimeout(()=>{
+            front[id].style.transform = 'rotateY(180deg)';
+            back[id].style.transform = 'rotateY(360deg)';
+        },500);
+    });
+}
+
+flipCard(0);
+
+
+
+//ESTO SI FUNCIONA
+/*
+
+
 front = document.getElementById('front');
 back = document.getElementById('back');
-
-front.style.transform = 'rotateY(0deg)';
-back.style.transform = 'rotateY(180deg)';
-
-console.log(card)
-
 card.addEventListener('click', (event)=>{
-    console.log(event)
+    // console.log(event)
+    console.log(event.srcElement.offsetParent.id)
     front.style.transform = 'rotateY(180deg)';
     back.style.transform = 'rotateY(360deg)';
     setTimeout(()=>{
@@ -51,3 +80,5 @@ card.addEventListener('click', (event)=>{
         back.style.transform = 'rotateY(180deg)';
     },1000);
 });
+
+*/
