2cf15fa440e4ea5ed7421e21663b381b27031c18
CodinGitHub <gms2programador@gmail.com>
2022-04-27 06:09:11 -0500

    showCard and flipCard function added

--- main.js
+++ main.js
@@ -2,9 +2,6 @@ let classList = [
   'nes-bulbasaur',
   'nes-charmander',
   'nes-squirtle',
-  'nes-bulbasaur',
-  'nes-charmander',
-  'nes-squirtle',
 ]
 
 card = document.getElementById('card')
@@ -12,10 +9,9 @@ front = document.getElementsByClassName('front');
 back = document.getElementsByClassName('back');
 
 // Card Generator
-
-for(i=0; i<classList.length; i++){
+for(i=0; i<classList.length*2; i++){
     cardGrid.innerHTML += `
-    <div class="card-container" id="card${i}">
+    <div class="card-container" id="${i}">
         <div class="card back nes-container is-rounded">
             <li class="nes-pokeball"></li>
         </div>
@@ -25,60 +21,28 @@ for(i=0; i<classList.length; i++){
     back[i].style.transform = 'rotateY(0deg)';   
 }
 
-/*
-cardGrid.innerHTML = `
-    <div class="card-container" id="card0">
-        <div class="card back nes-container is-rounded">
-            <li class="nes-pokeball"></li>
-        </div>
-        <div class="card front nes-container is-rounded"></div>
-    </div>
-`
-*/
-
+// Gettind card ID
 cardGrid.addEventListener('click', (event)=>{
-    console.log(event)
+    let id;
     if(event.srcElement.offsetParent.id == ''){
-        console.log ('hiciste clic en la imagen')
+        id = event.srcElement.offsetParent.offsetParent.id;
     }else{
-        console.log(event.srcElement.offsetParent.id)
+        id = event.srcElement.offsetParent.id;
     }
+    showCard(Number(id));
 });
 
 
-function flipCard (id){
-    currentCard = document.getElementById(`card${id}`)
-    currentCard.addEventListener('click', (event)=>{
-        // console.log(event)
-        // console.log(event.srcElement.offsetParent.id)
-        front[id].style.transform = 'rotateY(0deg)';
-        back[id].style.transform = 'rotateY(180deg)';
-        setTimeout(()=>{
-            front[id].style.transform = 'rotateY(180deg)';
-            back[id].style.transform = 'rotateY(360deg)';
-        },500);
-    });
+function showCard (id){
+    currentCard = document.getElementById(id);
+    front[id].style.transform = 'rotateY(0deg)';
+    back[id].style.transform = 'rotateY(180deg)';
 }
 
-flipCard(0);
-
-
-
-//ESTO SI FUNCIONA
-/*
-
-
-front = document.getElementById('front');
-back = document.getElementById('back');
-card.addEventListener('click', (event)=>{
-    // console.log(event)
-    console.log(event.srcElement.offsetParent.id)
-    front.style.transform = 'rotateY(180deg)';
-    back.style.transform = 'rotateY(360deg)';
+function flipCard(id){
+    currentCard = document.getElementById(id);
     setTimeout(()=>{
-        front.style.transform = 'rotateY(360deg)';
-        back.style.transform = 'rotateY(180deg)';
-    },1000);
-});
-
-*/
+        front[id].style.transform = 'rotateY(180deg)';
+        back[id].style.transform = 'rotateY(360deg)';
+    },500); 
+}
